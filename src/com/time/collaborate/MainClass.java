1602f2ca4c75a9649f22fec94b0798f199bd69ac
Kent Hervey <kent.hervey@gmail.com>
2019-08-09 18:35:59 -0500

    initial commit

---
+++ src/com/time/collaborate/MainClass.java
@@ -0,0 +1,11 @@
+package com.time.collaborate;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first line for collaboration");
+
+	}
+
+}
