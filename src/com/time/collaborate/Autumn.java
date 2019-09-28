b47103704141d5fb3f26b5eb84dce0bf588d9257
tonylangworthy <tony.langworthy@hotmail.com>
2019-09-28 11:59:20 -0500

    Added class Autumn

---
+++ src/com/time/collaborate/Autumn.java
@@ -0,0 +1,12 @@
+package com.time.collaborate;
+
+class Autumn {
+
+    private static String START_DATE = "September 23, 2019";
+
+    private static String START_TIME = "7:50 UTC";
+
+    static String getStartDateTime() {
+        return START_DATE + " " + START_TIME;
+    }
+}
