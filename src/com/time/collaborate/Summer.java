1afa54b8915795f21c5058008ba76ee339b593ba
tonylangworthy <tony.langworthy@hotmail.com>
2019-10-09 20:56:36 -0500

    Fixed Summer start date

--- src/com/time/collaborate/Summer.java
+++ src/com/time/collaborate/Summer.java
@@ -1,4 +1,14 @@
 package com.time.collaborate;
 
 public class Summer {
+
+    private static final String START_DATE = "June 21st, 2019";
+
+    public Summer() {
+
+    }
+
+    public String getSummerStartDate() {
+        return START_DATE;
+    }
 }
